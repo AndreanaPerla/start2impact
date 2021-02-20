33c9787ae6fec8e8f83bee8449bf99d95b0d8577
AndreanaPerla <68234455+AndreanaPerla@users.noreply.github.com>
2021-02-20 18:39:23 +0100

    Add files via upload

---
+++ Progetto Wordpress/inc/templates/contact-form.php
@@ -0,0 +1,25 @@
+<form id="sincontruContactForm" class="sincontru-contact-form" action="#" method="post">
+  <div class="form-group">
+    <input type="text" class="form-control sincontru-form-control"
+      placeholder="Nome..." id='name' name="name" required="required">
+  </div>
+
+  <div class="form-group">
+    <input type="email" class="form-control sincontru-form-control"
+      placeholder="Email..." id='email' name="email" required="required">
+  </div>
+
+  <div class="form-group">
+    <textarea name="message" id="message" class="form-control sincontru-form-control"
+      required="required" placeholder="Il tuo messaggio..."></textarea>
+  </div>
+
+  <div class="text-center">
+    <button type="submit" class="btn-sincontru-form btn btn-default btn-lg">Submit</button>
+  </div>
+</form>
+
+<div class="map text-center">
+  <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d750.7911984730437!2d9.10867608853711!3d39.21643594170919!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x12e735acc84847eb%3A0xc694ac580969ee95!2sCagliari%20Piazza%20del%20Carmine!5e0!3m2!1sit!2sit!4v1613052112447!5m2!1sit!2sit"
+    frameborder="0" style="border: 1px solid #433e3b;" allowfullscreen="" aria-hidden="false" tabindex="0"></iframe>
+</div>
